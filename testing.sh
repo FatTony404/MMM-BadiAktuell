16303b2fd889885c701fcee52370409731dfd43b
Andreas Pfeifer <andreas@typewriter.at>
2022-05-12 21:47:14 +0200

    some updates

--- testing.sh
+++ testing.sh
@@ -6,7 +6,6 @@ docker rm mm
 cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/mounts/modules/MMM-BadiAktuell"
 git checkout dev
 git pull
-npm install 
 cd "/Users/andreaspfeifer/Documents/GitHub/Docker/magicmirror/run"
 docker-compose up -d 
 docker logs -f mm
