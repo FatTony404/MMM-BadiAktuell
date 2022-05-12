4bc8ae8ece1a05a568fcecfb03150f9378a0c6f6
Andreas Pfeifer <andreas@typewriter.at>
2022-05-12 22:22:08 +0200

    updates

--- node_helper.js
+++ node_helper.js
@@ -25,7 +25,7 @@ module.exports = NodeHelper.create({
         if(response.status >= 200 && response.status <= 299){
           var content = await response.text();
           data = await parser.parseStringPromise(content);
-          console.log("fetched data "+data);
+          console.log(data);
           this.sendSocketNotification('CALENDAR_RESULT', data);
         } else {
           console.log(response.status, response.statusText);
@@ -38,55 +38,55 @@ module.exports = NodeHelper.create({
     })();
   },  
 
-  // old function
-  getCalendarData: function(payload){
-    var startdate = moment().format('YYYY-MM-DD');
-    var enddate = moment(startdate, 'YYYY-MM-DD').add(payload.daysToDisplay, 'days').format('YYYY-MM-DD');
+  // // old function
+  // getCalendarData: function(payload){
+  //   var startdate = moment().format('YYYY-MM-DD');
+  //   var enddate = moment(startdate, 'YYYY-MM-DD').add(payload.daysToDisplay, 'days').format('YYYY-MM-DD');
 
-    var url = new URL(payload.url);
+  //   var url = new URL(payload.url);
 
-    var params = new URLSearchParams();
-    params.append('zip', payload.zipCode);
-    params.append('start', startdate);
-    params.append('end', enddate);
-    params.append('sort', payload.sort);
-    if(payload.showTypes != ""){
-      for(var i = 0; i<payload.showTypes.length; i++){
-        params.append('types', payload.showTypes[i]);
-      }
-    }
-    params.append('limit', payload.limitEntries);
+  //   var params = new URLSearchParams();
+  //   params.append('zip', payload.zipCode);
+  //   params.append('start', startdate);
+  //   params.append('end', enddate);
+  //   params.append('sort', payload.sort);
+  //   if(payload.showTypes != ""){
+  //     for(var i = 0; i<payload.showTypes.length; i++){
+  //       params.append('types', payload.showTypes[i]);
+  //     }
+  //   }
+  //   params.append('limit', payload.limitEntries);
     
-    url.search = params.toString();
+  //   url.search = params.toString();
 
-    (async () => {
-      try {
-        var response = await fetch(url, {
-          method: 'GET', 
-          headers: {'Content-Type': 'application/json'}, 
-        });
-        var json = "";
-        if(response.status >= 200 && response.status <= 299){
-          json = await response.json();        
-          this.sendSocketNotification('CALENDAR_RESULT', json);
-        } else {
-          console.log(response.status, response.statusText);
-          this.sendSocketNotification('CALENDAR_ERROR', "error");
-        }
-      } catch (error){
-        console.log(error);
-      }
-    })();
-  },
+  //   (async () => {
+  //     try {
+  //       var response = await fetch(url, {
+  //         method: 'GET', 
+  //         headers: {'Content-Type': 'application/json'}, 
+  //       });
+  //       var json = "";
+  //       if(response.status >= 200 && response.status <= 299){
+  //         json = await response.json();        
+  //         this.sendSocketNotification('CALENDAR_RESULT', json);
+  //       } else {
+  //         console.log(response.status, response.statusText);
+  //         this.sendSocketNotification('CALENDAR_ERROR', "error");
+  //       }
+  //     } catch (error){
+  //       console.log(error);
+  //     }
+  //   })();
+  // },
 
 
 
   socketNotificationReceived: function(notification, payload) {
     console.log(this.name + " received a socket notification: " + notification + " - Payload: " + payload);
 
-    if(notification == "CALENDAR_GET"){
-      this.getCalendarData(payload);
-    }
+    // if(notification == "CALENDAR_GET"){
+    //   this.getCalendarData(payload);
+    // }
 
     if(notification == "TEMP_GET"){
       this.getTemperatureData(payload);
